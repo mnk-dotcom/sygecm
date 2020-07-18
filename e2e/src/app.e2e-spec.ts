74249bcf6d11749ee146ce09ed2e9b5145fad348
Abdennasser ESSATI <abdennasser.essati@4d.com>
2020-07-18 20:46:16 +0100

    chore: initial commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('sygecm app is running!');
+    expect(page.getTitleText()).toEqual('angularjwtauth app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sygecm'`, () => {
+  it(`should have as title 'angularjwtauth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sygecm');
+    expect(app.title).toEqual('angularjwtauth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sygecm app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angularjwtauth app is running!');
   });
 });
